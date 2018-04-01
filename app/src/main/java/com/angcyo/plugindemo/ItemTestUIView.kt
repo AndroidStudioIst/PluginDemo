ec81bf5828c364a38df141736571ca30e4db12e1
angcyo <angcyo@126.com>
2018-04-01 21:28:37 +0800

    init

--- app/src/main/java/com/angcyo/plugindemo/ItemTestUIView.kt
+++ app/src/main/java/com/angcyo/plugindemo/ItemTestUIView.kt
@@ -1,10 +1,8 @@
 package com.angcyo.plugindemo
 
 import android.graphics.Color
-import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import android.widget.TextView
 import com.angcyo.uiview.base.Item
 import com.angcyo.uiview.base.SingleItem
@@ -20,7 +18,12 @@ class ItemTestUIView : BaseItemUIView() {
         items.add(object : SingleItem() {
             override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
                 baseInitItem(holder, "TestItem....11...$isInPlugin") {
-                    UILoading.flow(mParentILayout).setLoadingTipText("插件点击...")
+                    val tip = if (isInPlugin) {
+                        mPluginPackage.getString(R.string.app_name2)
+                    } else {
+                        "no"
+                    }
+                    UILoading.flow(mParentILayout).setLoadingTipText("插件点击...$tip")
                 }
             }
         })
@@ -39,9 +42,7 @@ class ItemTestUIView : BaseItemUIView() {
                     TextView(mActivity).apply {
                         text = "动态创建的TextView"
                         setBackgroundColor(Color.RED)
-                        parent?.addView(this, FrameLayout.LayoutParams(-2, -2).apply {
-                            gravity = Gravity.CENTER
-                        })
+                        parent?.addView(this, ViewGroup.LayoutParams(-2, -2))
                     }
                 } else {
                     null
@@ -71,7 +72,7 @@ class ItemTestUIView : BaseItemUIView() {
         items.add(object : SingleItem() {
             override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
                 baseInitItem(holder, "TestItem........") {
-                    UILoading.progress(mParentILayout).setLoadingTipText("插件点击...")
+                    UILoading.progress(mParentILayout).setLoadingTipText("插件点击...${getString(R.string.app_name)}")
                 }
             }
         })
