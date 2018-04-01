5a760e5f0412857430b53f6b40459c250288fec3
angcyo <angcyo@126.com>
2018-04-01 21:53:11 +0800

    init

--- app/src/main/java/com/angcyo/plugindemo/ItemTestUIView.kt
+++ app/src/main/java/com/angcyo/plugindemo/ItemTestUIView.kt
@@ -4,11 +4,13 @@ import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.angcyo.library.utils.L
 import com.angcyo.uiview.base.Item
 import com.angcyo.uiview.base.SingleItem
 import com.angcyo.uiview.base.UIItemUIView.baseInitItem
 import com.angcyo.uiview.dialog.UILoading
 import com.angcyo.uiview.recycler.RBaseViewHolder
+import com.angcyo.uiview.utils.T_
 
 /**
  * Created by angcyo on 2018/04/01 19:41
@@ -34,22 +36,25 @@ class ItemTestUIView : BaseItemUIView() {
             }
 
             override fun getItemLayoutId(): Int {
+                L.e("Plugin: call: getItemLayoutId -> ")
                 return R.layout.item_test
             }
 
             override fun createItemView(parent: ViewGroup?, viewType: Int): View? {
                 return if (isInPlugin) {
+                    L.e("Plugin: call: createItemView -> ")
                     TextView(mActivity).apply {
                         text = "动态创建的TextView"
                         setBackgroundColor(Color.RED)
                         parent?.addView(this, ViewGroup.LayoutParams(-2, -2))
+
+                        T_.info("创建Item in $parent")
                     }
                 } else {
                     null
                 }
             }
         })
-
 
         items.add(object : SingleItem() {
             override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
